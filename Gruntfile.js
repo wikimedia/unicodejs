1a3ff3a6d84a59bbc6c64deaf423a8acb7d4c897
James D. Forrester <jforrester@wikimedia.org>
2014-08-09 15:53:05 +0100

    build: Make into a built library, set up test infrastructure
    Imported test infrastructure from the VisualEditor repository where
    this library used to be maintained.
    
    Change-Id: I2ddd10bba5440300915b43b548ad0acab79ceab0

---
+++ Gruntfile.js
@@ -0,0 +1,88 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-contrib-concat' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+	grunt.loadNpmTasks( 'grunt-karma' );
+	grunt.loadTasks( 'build/tasks' );
+
+	var modules = grunt.file.readJSON( 'build/modules.json' ),
+		moduleUtils = require( './build/moduleUtils' ),
+		srcFiles = moduleUtils.makeBuildList( modules, [ 'unicodejs' ] ).scripts,
+		testFiles = moduleUtils.makeBuildList( modules, [ 'unicodejs.tests' ] ).scripts;
+
+	// We want to use `grunt watch` to start this and karma watch together.
+	grunt.renameTask( 'watch', 'runwatch' );
+
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' ),
+		clean: {
+			dist: [ 'dist/*', 'docs/*' ]
+		},
+		concat: {
+			all: {
+				options: {
+					banner: grunt.file.read( 'build/banner.txt' )
+				},
+				dest: 'dist/unicodejs.js',
+				src: srcFiles
+			}
+		},
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'*.js',
+				'{docs,build,src,tests}/**/*.js'
+			]
+		},
+		jscs: {
+			src: '<%= jshint.all %>'
+		},
+		karma: {
+			options: {
+				files: testFiles,
+				frameworks: [ 'qunit' ],
+				reporters: [ 'dots' ],
+				singleRun: true,
+				autoWatch: false
+			},
+			phantomjs: {
+				browsers: [ 'PhantomJS' ],
+				preprocessors: {
+					'src/*.js': [ 'coverage' ]
+				},
+				reporters: [ 'dots', 'coverage' ],
+				coverageReporter: { reporters: [
+					{ type: 'html', dir: 'docs/coverage' },
+					{ type: 'text-summary', dir: 'docs/coverage' }
+				] }
+			},
+			local: {
+				browsers: [ 'Firefox', 'Chrome' ]
+			},
+			bg: {
+				browsers: [ 'PhantomJS', 'Firefox', 'Chrome' ],
+				singleRun: false,
+				background: true
+			}
+		},
+		runwatch: {
+			files: [
+				'.{jscsrc,jshintignore,jshintrc}',
+				'<%= jshint.all %>'
+			],
+			tasks: [ 'test', 'karma:bg:run' ]
+		}
+	} );
+
+	grunt.registerTask( 'build', [ 'clean', 'git-build', 'concat' ] );
+	grunt.registerTask( 'lint', [ 'jshint', 'jscs' ] );
+	grunt.registerTask( 'unit', [ 'karma:phantomjs' ] );
+	grunt.registerTask( 'test', [ 'build', 'lint', 'unit' ] );
+	grunt.registerTask( 'watch', [ 'karma:bg:start', 'runwatch' ] );
+	grunt.registerTask( 'default', 'test' );
+};
--- src/unicodejs.js
+++ src/unicodejs.js
@@ -211,5 +211,6 @@
 	};
 
 	// Expose
+	/*jshint browser:true */
 	window.unicodeJS = unicodeJS;
 }() );
