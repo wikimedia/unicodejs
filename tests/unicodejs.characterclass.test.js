51597a5ebbf5665d0da02c0a672e0867b3515696
David Chan <david@sheetmusic.org.uk>
2015-01-15 08:50:41 +0800

    Word character class regex
    Use Unicode data to build the character class equivalent to \w , based on the
    recommendation in http://unicode.org/reports/tr18/#Compatibility_Properties .
    
    unicodejs-properties.py:
    * Refactor extraction to be more reusable
    * Merge consecutive ranges
    * Express ranges of length 2 as a pair of singletons, to save space
    * Extract from DerivedCoreProperty.txt and DerivedGeneralCategory.txt
    
    unicodejs.characterclass.js:
    * "word" character class regex string
    
    unicodejs.derivedcoreproperties.js, unicodejs.derivedgeneralcategories.js:
    * Extracted data
    
    unicodejs.characterclass.test.js
    * Tests of the "word" regex including non-BMP cases
    
    Change-Id: I1b785a369a2c80f6afc27f792787f6590948241e

---
+++ tests/unicodejs.characterclass.test.js
@@ -0,0 +1,55 @@
+/*!
+ * UnicodeJS character class module tests
+ *
+ * @copyright 2015 UnicodeJS team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+
+QUnit.module( 'unicodeJS.characterclass' );
+
+QUnit.test( 'patterns', function ( assert ) {
+	var wordChars, nonWordChars,
+		wordGlobalRegex = new RegExp( unicodeJS.characterclass.patterns.word, 'g' );
+
+	wordChars = [
+		// Basic Latin letter
+		'a',
+		// Basic Latin number
+		'1',
+		// Underscore (Punctuation, connector)
+		'_',
+		// Latin-1 Supplement letter
+		'é',
+		// Latin Extended-A letter
+		'ŵ',
+		// Mark (combining accent)
+		'x', '\u0301',
+		// Han character
+		'中',
+		// Full-width punctuation connector
+		'︴',
+		// SMP Han character U+282E2
+		'𨋢',
+		// Combining mark and ZWNJ
+		'क', '\u094d', '\u200C', 'ष',
+		// SMP math letter U+1D538
+		'𝔸'
+	];
+
+	nonWordChars = [
+		// Basic Latin punctuation
+		'$', '-', '.', '!', ' ', '"', '\'', '(', ')', '[', ']', '{', '}',
+		// Other punctuation
+		'“', '”', '‘', '’', '「', '」',
+		// Space
+		' ', '\r', '\n', '\t', '\u3000',
+		// Non-Basic-Latin digit U+0668 and U+1D7E0
+		'٨',
+		// SMP emoticon U+1F607 and digit U+1D7E0
+		'😇', '𝟠'
+	];
+
+	QUnit.expect( 2 );
+	assert.deepEqual( wordChars.join( '' ).match( wordGlobalRegex ), wordChars );
+	assert.strictEqual( nonWordChars.join( '' ).match( wordGlobalRegex ), null );
+});
